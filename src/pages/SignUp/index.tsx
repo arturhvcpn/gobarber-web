64b105f1f4f6dc47ac8987a03418a359312f5d94
Artur <arturcastropolonorte@gmail.com>
2020-05-30 19:07:01 -0300

    inserted unform {@unform/core && unform/web}

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiMail, FiLock,FiUser, FiArrowLeft} from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles'
 import logoImg from '../../assets/logo.svg';
@@ -7,13 +8,18 @@ import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-const SignUp: React.FC = () => (
-  <Container>
+const SignUp: React.FC = () => {
+  function handleSubmit(data:object):void{
+    console.log(data)
+
+  }
+  return(
+    <Container>
     <Background/>
       < Content>
       <img src={logoImg} alt="GoBarber"/>
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h1>Faça seu Cadastro</h1>
 
         <Input icon={FiUser} name= "name" placeholder="Name"/>
@@ -22,7 +28,7 @@ const SignUp: React.FC = () => (
         <Input icon={FiLock} name= "password" type="password" placeholder="Senha"/>
 
         <Button type="submit">Cadastrar</Button>
-      </form>
+      </Form>
 
       <a href="login">
         <FiArrowLeft/>
@@ -31,7 +37,8 @@ const SignUp: React.FC = () => (
 
       </Content>
   </Container>
+  );
+}
 
-);
 
 export default SignUp;
