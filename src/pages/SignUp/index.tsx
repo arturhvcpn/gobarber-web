481bc593c5a43fa01f35b7d7f3e905bb1594aae4
Artur <arturcastropolonorte@gmail.com>
2020-06-10 22:35:14 -0300

    formRef using @unform/core

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiMail, FiLock,FiUser, FiArrowLeft} from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
@@ -10,6 +11,9 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
+
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback( async (data:object) =>{
     try {
       const schema = Yup.object().shape({
@@ -19,11 +23,13 @@ const SignUp: React.FC = () => {
       });
 
       await schema.validate(data,{
-      abortEarly:false,
-    });
+        abortEarly:false,
+      });
+
 
     } catch (error) {
       console.log(error);
+      // formRef.current?.set()
     }
   },[]);
   return(
@@ -32,7 +38,7 @@ const SignUp: React.FC = () => {
       < Content>
       <img src={logoImg} alt="GoBarber"/>
 
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef}onSubmit={handleSubmit}>
         <h1>Faça seu Cadastro</h1>
 
         <Input icon={FiUser} name= "name" placeholder="Name"/>
