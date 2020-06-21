a36968c92ceeaaa04b9cb0f4b057dc8e1194e1a8
Artur <arturcastropolonorte@gmail.com>
2020-06-20 23:12:51 -0300

    insertion navigation with autentication, and messages with error and success

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,11 +1,15 @@
 import React, { useCallback, useRef } from 'react';
 import { FiMail, FiLock,FiUser, FiArrowLeft} from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 
 import * as Yup from 'yup';
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast' ;
 
 import getValidationErrors from '../../utils/getValidationErros';
 
@@ -15,11 +19,19 @@ import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
+interface SignUpFormData{
+  name:string;
+  email:string;
+  password:string;
+}
+
 const SignUp: React.FC = () => {
 
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback( async (data:object) =>{
+  const handleSubmit = useCallback( async (data: SignUpFormData) =>{
     try {
 
       const schema = Yup.object().shape({
@@ -32,13 +44,32 @@ const SignUp: React.FC = () => {
         abortEarly:false,
       });
 
+      await api.post('/users',data);
+
+      history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro Realizado',
+        description: 'Você já pode fazer seu logon no GoBarber!',
+      });
+
     } catch (err) {
+      if(err instanceof Yup.ValidationError){
+        const errors = getValidationErrors(err);
 
-      const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
 
-      formRef.current?.setErrors(errors);
+        return;
+      }
+      addToast({
+        type: 'error',
+        title: 'Erro na Cadastro',
+        description: 'Ocorreu um erro ao realizar o cadastro, tente novamente.',
+      });
     }
-  },[]);
+
+  },[addToast, history]);
   return(
     <Container>
     <Background/>
